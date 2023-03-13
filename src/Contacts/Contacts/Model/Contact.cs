c5a28137c8abbeca76e8324ebbc78ebd4d5ef25f
YaroslavEgunov517-1 <99376130+YaroslavEgunov517-1@users.noreply.github.com>
2023-03-13 21:20:32 +0700

    Как, а главное почему нельзя добавить 2 элемента в 1 group box

---
+++ src/Contacts/Contacts/Model/Contact.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contacts.Model
+{
+    public class Contact
+    {
+        private string _name;
+
+        private string _phoneNumber;
+
+        private string _email;
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+            }
+        }
+
+        public string PhoneNumber
+        {
+            get
+            {
+                return _phoneNumber;
+            }
+            set
+            {
+                _phoneNumber = value;
+            }
+        }
+
+        public string Email
+        {
+            get
+            {
+                return _email;
+            }
+            set
+            {
+                _email = value;
+            }
+        }
+
+        public Contact(string name, string phoneNumber, string email)
+        {
+            Name = name;
+            PhoneNumber = phoneNumber;
+            Email = email;
+        }
+
+        public Contact()
+        {
+            Name = "None";
+            PhoneNumber = "";
+            Email = "";
+        }
+    }
+}
